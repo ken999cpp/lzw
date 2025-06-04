9429b021fe7857b8276d27ee168a042c75c3bbfe
yaya666 <rostislavkalinicenko7@gmail.com>
2025-06-05 01:27:26 +0300

    init

---
+++ lzw.cpp
@@ -0,0 +1,40 @@
+#include "lzw.h"
+#include <iostream>
+
+std::vector<int> encoding(std::string s1)
+{
+    std::unordered_map<std::string, int> dict;
+    for (int i = 0; i < 256; ++i) {dict[std::string(1, static_cast<char>(i))] = i;}
+    std::string current;
+    std::vector<int> res;
+    int code = 256;
+    for (auto& ch : s1)
+    {
+        std::string next = current + ch;
+        if (dict.find(next) != dict.end()) {current = next;}
+        else
+        {
+            res.push_back(dict[current]);
+            dict[next] = code++;
+            current = std::string(1, ch);
+        }
+    }
+    if (!current.empty()) {res.push_back(dict[current]);}
+    return res;
+}
+void decoding(std::vector<int> op)
+{
+    if (op.empty()) {return;}
+    std::unordered_map<int, std::string> dict;
+    for (int i = 0; i < 256; ++i) {dict[i] = std::string(1, static_cast<char>(i));}
+    int code = 256;
+    std::string prev, current;
+    for (auto& c : op)
+    {   
+        if (dict.find(c) != dict.end()) {current = dict[c];}
+        else {current = prev + prev[0];}
+        std::cout << current;
+        if (!prev.empty()) {dict[code++] = prev + current[0];}
+        prev = current;
+    }
+}
---
+++ lzw.h
@@ -0,0 +1,14 @@
+#ifndef INC_3_LZW_H
+#define INC_3_LZW_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+//! Функция кодирования
+std::vector<int> encoding(std::string s1);
+
+//! Функция декодирования
+void decoding(std::vector<int> op);
+
+#endif //INC_3_LZW_H
