a435d80c6a408f6be545ce23055bd8745f8966d6
ken999cpp <dadayadayadada@gmail.com>
2025-06-05 08:51:25 +0300

    Update lzw.cpp

--- lzw.cpp
+++ lzw.cpp
@@ -3,6 +3,12 @@
 
 std::vector<int> encoding(std::string s1)
 {
+    std::cout << "Encoding..." << std::endl;
+    if (s1.empty())
+    {
+        std::cout << "Nothing to encode" << std::endl;
+        return {};
+    }
     std::unordered_map<std::string, int> dict;
     for (int i = 0; i < 256; ++i) {dict[std::string(1, static_cast<char>(i))] = i;}
     std::string current;
@@ -10,6 +16,7 @@ std::vector<int> encoding(std::string s1)
     int code = 256;
     for (auto& ch : s1)
     {
+        if (code > 4095) {break;}
         std::string next = current + ch;
         if (dict.find(next) != dict.end()) {current = next;}
         else
@@ -24,18 +31,23 @@ std::vector<int> encoding(std::string s1)
 }
 void decoding(std::vector<int> op)
 {
-    if (op.empty()) {return;}
+    std::cout << "Decoding..." << std::endl;
+    if (op.empty())
+    {
+        std::cout << "Nothing to decode" << std::endl;
+        return;
+    }
     std::unordered_map<int, std::string> dict;
     for (int i = 0; i < 256; ++i) {dict[i] = std::string(1, static_cast<char>(i));}
     int code = 256;
     std::string prev, current;
     for (auto& c : op)
     {   
+        if (code > 4095) {break;}
         if (dict.find(c) != dict.end()) {current = dict[c];}
         else {current = prev + prev[0];}
         std::cout << current;
         if (!prev.empty()) {dict[code++] = prev + current[0];}
         prev = current;
-        if (code > 4095) {break;}
     }
 }
